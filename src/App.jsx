015cd12a889359322d1001559cf0bf7153c144e7
Tegar Witjaksono <rmgaming601@gmail.com>
2024-12-07 23:45:33 +0700

    tictac use react js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import TicTacToe from './components/TicTacToe/TicTacToe'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <TicTacToe/>
+      </div>
+    </>
+  )
+}
+
+export default App
